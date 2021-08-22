216336c3747ade0deac00255196962c893727f20
yahyaayabakan <ayabakanyahya@gmail.com>
2021-08-22 18:05:33 +0100

    Created framework from scratch

---
+++ src/test/java/com/navfort/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.navfort.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports"},
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false,
+        tags = "@login"
+)
+public class CukesRunner {
+}
---
+++ src/test/java/com/navfort/utilities/StringUtills.java
@@ -0,0 +1,4 @@
+package com.navfort.utilities;
+
+public class StringUtills {
+}
