82bf3fd3b193909c628de414d3b3989ad3d63735
yahyaayabakan <ayabakanyahya@gmail.com>
2021-08-25 17:06:44 +0100

    added extra pages

--- src/test/java/com/navfort/stepdefs/Hooks.java
+++ src/test/java/com/navfort/stepdefs/Hooks.java
@@ -1,9 +1,15 @@
 package com.navfort.stepdefs;
 
+import com.navfort.pages.ContactsPage;
+import com.navfort.pages.DashboardPage;
+import com.navfort.utilities.BrowserUtils;
 import com.navfort.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -41,5 +47,57 @@ public class Hooks {
     }
 
 
+    public static class NavigationMenuStepDefs {
+        @When("the user navigates to Fleet, Vehicles")
+        public void the_user_navigates_to_Fleet_Vehicles() {
+            //selenium code
+            System.out.println("the user navigates to Fleet, Vehicles");
+        }
 
+        @Then("the title should be Vehicles")
+        public void the_title_should_be_Vehicles() {
+            System.out.println("Expected and Actual title are matching");
+        }
+
+        @When("the user navigates to Marketing, Campaigns")
+        public void the_user_navigates_to_Marketing_Campaigns() {
+            System.out.println("the user navigates to Marketing, Campaigns");
+
+        }
+
+        @Then("title should be Campaigns")
+        public void title_should_be_Campaigns() {
+            System.out.println("Expected and Actual title are matching");
+        }
+
+        @When("the user navigates to Activities, Calendar Events")
+        public void the_user_navigates_to_Activities_Calendar_Events() {
+            System.out.println("the user navigates to Activities, Calendar Events");
+
+        }
+
+        @Then("title should be Calendars")
+        public void title_should_be_Calendars() {
+            System.out.println("Expected and Actual title are matching");
+        }
+
+
+        @When("the user navigates to {string} {string}")
+        public void the_user_navigates_to(String tab, String module) {
+            new DashboardPage().navigateToModule(tab,module);
+        }
+
+
+        @Then("default page number should be {int}")
+        public void default_page_number_should_be(Integer expectedPageNum) {
+            BrowserUtils.waitFor(3);
+            ContactsPage contactsPage = new ContactsPage();
+            Integer actualNumber =Integer.parseInt(contactsPage.pageNumber.getAttribute("value"));
+
+            Assert.assertEquals(expectedPageNum,actualNumber);
+        }
+
+
+
+    }
 }
