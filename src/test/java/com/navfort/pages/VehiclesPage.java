7ab619efe26b7534a6b7fb15e9f6617177435572
yahyaayabakan <ayabakanyahya@gmail.com>
2021-08-25 19:31:45 +0100

    first scenario is done

--- src/test/java/com/navfort/pages/VehiclesPage.java
+++ src/test/java/com/navfort/pages/VehiclesPage.java
@@ -3,7 +3,14 @@ package com.navfort.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class VehiclesPage {
+import java.util.List;
+
+public class VehiclesPage extends BasePage{
     @FindBy(xpath = "//i[@class='fa-cog hide-text']")
     public WebElement gridSettingsButton;
+
+    @FindBy(css = ".title-cell")
+    public List<WebElement> columnNames;
+
+
 }
--- src/test/java/com/navfort/stepdefs/GridSettingsStepDefs.java
+++ src/test/java/com/navfort/stepdefs/GridSettingsStepDefs.java
@@ -1,4 +1,27 @@
 package com.navfort.stepdefs;
 
+import com.navfort.pages.VehiclesPage;
+import com.navfort.utilities.BrowserUtils;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
 public class GridSettingsStepDefs {
+    @When("the user clicks to grid settings button")
+    public void the_user_clicks_to_grid_settings_button() {
+        VehiclesPage vehiclesPage = new VehiclesPage();
+        BrowserUtils.waitFor(12);
+        vehiclesPage.gridSettingsButton.click();
+    }
+
+    @Then("Column names in grid settings should include following options")
+    public void column_names_in_grid_settings_should_include_following_options(List<String> gridOptions) {
+        BrowserUtils.waitFor(6);
+        List<String> actualOptions = BrowserUtils.getElementsText(new VehiclesPage().columnNames);
+        Assert.assertEquals(actualOptions,gridOptions);
+    }
+
 }
