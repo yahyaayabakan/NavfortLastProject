da2d298ee6cf677c576d1e5311430d39a71ec7b8
LauraIstoc <lla_ura@yahoo.com>
2021-09-01 20:30:09 +0100

    Created General Information Page

---
+++ src/test/java/com/navfort/pages/GeneralInfoVehiclePage.java
@@ -0,0 +1,46 @@
+package com.navfort.pages;
+
+import com.navfort.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class GeneralInfoVehiclePage {
+
+    public GeneralInfoVehiclePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(className = "user-name")
+    public WebElement contactFullName;
+
+    @FindBy(css = ".pull-left.btn-group.icons-holder>a")
+    public WebElement addEventButton;
+
+    @FindBy(xpath = "//div[@class='pull-right']/div/div/a[@title='Edit Car']")
+    public WebElement editButton;
+
+    @FindBy(css = ".btn.icons-holder-text.no-hash.remove-button")
+    public WebElement deleteButton;
+
+    public WebElement cellNameLocation(String cellName) {
+
+        String xpath = "//div[@class='grid-container']//tbody/tr/td[contains(text(),'" + cellName + "')and @data-column-label='Location']";
+        return Driver.get().findElement(By.xpath(xpath));
+    }
+
+    public void EditDeleteAddEventBtns( ){
+        List<WebElement> pathButtons =Driver.get().findElements(By.xpath(
+                "//div[@class='pull-right title-buttons-container']/div/a"));
+        for(WebElement buttons : pathButtons){
+            Assert.assertTrue(buttons.isDisplayed());
+        }
+    }
+
+
+
+}
