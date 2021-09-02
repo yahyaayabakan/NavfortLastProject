0bf4c4a64299c294b1628259406519564810f39f
emanuelasavi <asaviemanuel@gmail.com>
2021-09-02 18:04:33 +0100

    Last update to the brunch

--- src/test/java/com/navfort/pages/AddEvent_1_Page.java
+++ src/test/java/com/navfort/pages/AddEvent_1_Page.java
@@ -16,22 +16,64 @@ public class AddEvent_1_Page extends BasePage{
     @FindBy(xpath = "//div[@class='control-label wrap']//label")
     public List<WebElement> allFieldsInTheAddEventWindow;
 
+    @FindBy(xpath = "//input[@name='oro_calendar_event_form[title]']")
+    public WebElement titleTextBox;
+
+    @FindBy(xpath = "//input[@name='oro_calendar_event_form[organizerDisplayName]']")
+    public WebElement organizeDisplayNameTextBox;
+
+    @FindBy(xpath = "//input[@name='oro_calendar_event_form[organizerEmail]']")
+    public WebElement organizerEmailTextBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//span[@class='validation-failed']/span/span")
+    public WebElement errorMessage;
+
+    @FindBy(xpath = "//button[@title='close']")
+    public WebElement closeEventTab;
+
+
+    @FindBy(xpath = "//div[@class='flash-messages-holder']//div[@class='message']")
+    public WebElement successfullyAddEvent;
     public void selectDriverByName(String driverName) {
+        BrowserUtils.waitForPageToLoad(10);
         String driverLocator = "//td[contains(text(),'"+ driverName +"')]";
         WebElement driverElement = Driver.get().findElement(By.xpath(driverLocator));
+        BrowserUtils.waitForClickablility(driverElement,10);
         driverElement.click();
     }
 
     public void selectEditDeleteAddEventModule(String module) {
+        BrowserUtils.waitForPageToLoad(10);
         String moduleLocator = "//div[@class='pull-right']//a[contains(text(),'"+ module +"')]";
         WebElement moduleElement = Driver.get().findElement(By.xpath(moduleLocator));
+        BrowserUtils.waitForClickablility(moduleElement,10);
         moduleElement.click();
     }
 
-    public void fieldsInAddEventWindows(String field) {
-        String fieldLocator = "//div[@class='control-label wrap']//label[contains(text(),'"+ field +"')]";
-        WebElement fieldElement = Driver.get().findElement(By.xpath(fieldLocator));
+    public void setCloseEventTab(){
+        BrowserUtils.waitForPageToLoad(10);
+        List<WebElement> closeBtn = Driver.get().findElements(By.xpath("//button[@title='close']"));
+        try {
+            for (WebElement webElement : closeBtn) {
+                if (webElement.isDisplayed()){
+                    webElement.click();
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public WebElement descriptionBox() {
+        WebElement iframeElement = Driver.get().findElement((By.tagName("iframe")));
+        Driver.get().switchTo().frame(iframeElement);
+        return Driver.get().findElement(By.xpath("//body[@id='tinymce']//p"));
+    }
+
+
 
 
 
